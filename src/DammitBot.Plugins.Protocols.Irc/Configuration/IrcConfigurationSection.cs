116176a7eaca7dd1dbe654c7ad1d7cbf89603d5a
Jason Duncan <Jason Duncan>
2017-03-01 19:13:24 -0500

    Updated the config example. Did a other thing.

--- src/DammitBot.Plugins.Protocols.Irc/Configuration/IrcConfigurationSection.cs
+++ src/DammitBot.Plugins.Protocols.Irc/Configuration/IrcConfigurationSection.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Configuration;
 
 namespace DammitBot.Protocols.Irc.Configuration
 {
@@ -27,23 +22,23 @@ namespace DammitBot.Protocols.Irc.Configuration
         #region Properties
 
         [ConfigurationProperty(Keys.SERVER, IsRequired = true)]
-        public virtual string Server => (string)this[Keys.SERVER];
+        public virtual string Server => (string) this[Keys.SERVER];
 
         [ConfigurationProperty(Keys.NICK)]
         public virtual string Nick
         {
             get
             {
-                var nick = (string)this[Keys.NICK];
+                var nick = (string) this[Keys.NICK];
                 return string.IsNullOrWhiteSpace(nick) ? User : nick;
             }
         }
 
         [ConfigurationProperty(Keys.USER, IsRequired = true)]
-        public virtual string User => (string)this[Keys.USER];
+        public virtual string User => (string) this[Keys.USER];
 
         [ConfigurationProperty(Keys.CHANNELS, IsRequired = true)]
-        public virtual string Channels => (string)this[Keys.CHANNELS];
+        public virtual string Channels => (string) this[Keys.CHANNELS];
 
         #endregion
     }
