9c0b8a7c598a544c84887f022f9fa1f52175354f
Jason Duncan <functionreturnfunction@gmail.com>
2017-03-08 08:19:42 -0500

    Added the intended date/time of reminders to the message for cases where the bot missed its mark from having been shut down or whatever.

--- src/DammitBot.Tests/Utilities/ReminderTextGeneratorTest.cs
+++ src/DammitBot.Tests/Utilities/ReminderTextGeneratorTest.cs
@@ -15,8 +15,10 @@ namespace DammitBot.Utilities
                 Username = "foo"
             };
 
-            Assert.Equal($"@{user.Username} you wanted me to remind you to do stuff and junk",
-                _target.Generate(new Reminder {Text = "to do stuff and junk", From = user, To = user}).Text);
+            Assert.Equal($"@{user.Username} you wanted me to remind you to do stuff and junk on {_now:d} at {_now:t}",
+                _target.Generate(new Reminder {
+                    Text = "to do stuff and junk", From = user, To = user, RemindAt = _now
+                }).Text);
         }
 
         [Fact]
@@ -29,8 +31,10 @@ namespace DammitBot.Utilities
                 Username = "bar"
             };
 
-            Assert.Equal($"@{differentUser.Username} {user.Username} wanted me to remind you to do stuff and junk",
-                _target.Generate(new Reminder {Text = "to do stuff and junk", From = user, To = differentUser}).Text);
+            Assert.Equal($"@{differentUser.Username} {user.Username} wanted me to remind you to do stuff and junk on {_now:d} at {_now:t}",
+                _target.Generate(new Reminder {
+                    Text = "to do stuff and junk", From = user, To = differentUser, RemindAt = _now
+                }).Text);
         }
 
         #endregion
