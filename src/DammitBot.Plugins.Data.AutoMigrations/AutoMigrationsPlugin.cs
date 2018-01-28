3c46d957951c93600610614946352cdd022641af
Jason Duncan <functionreturnfunction@gmail.com>
2018-01-27 19:22:29 -0500

    Working on (temporary) migrations.

---
+++ src/DammitBot.Plugins.Data.AutoMigrations/AutoMigrationsPlugin.cs
@@ -0,0 +1,29 @@
+﻿using System.Data;
+using DammitBot.Abstract;
+
+namespace DammitBot.Plugins.Data.AutoMigrations
+{
+    public class AutoMigrationsPlugin : IPlugin
+    {
+        protected readonly IDbConnection _connection;
+
+        public AutoMigrationsPlugin(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public void Initialize()
+        {
+            var cmd = _connection.CreateCommand();
+            cmd.CommandText = "SELECT name FROM sqlite_master WHERE type='table' AND name='nicks'";
+            if (cmd.ExecuteScalar() != null)
+            {
+                return;
+            }
+
+            cmd.CommandText = "";
+        }
+
+        public void Cleanup() {}
+    }
+}
