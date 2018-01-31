773f2c8db8cd3d1722cd1d4b31b2414e1a6ddef4
Jason Duncan <functionreturnfunction@gmail.com>
2018-01-31 06:44:18 -0500

    More tests passing.

--- src/DammitBot.Plugins.Data/Models/Message.cs
+++ src/DammitBot.Plugins.Data/Models/Message.cs
@@ -12,7 +12,7 @@ namespace DammitBot.Data.Models
         public virtual string Protocol { get; set; }
         public virtual string Channel { get; set; }
         public virtual Nick From { get; set; }
-        public virtual int FromId { get; set; }
+        public virtual int? FromId { get; set; }
 
         public virtual DateTime CreatedAt { get; set; }
         public virtual DateTime? UpdatedAt { get; set; }
