a13c5638e0a4684af2d09f02af40aad68f209d93
Jason Duncan <functionreturnfunction@gmail.com>
2023-08-04 17:40:34 -0400

    Rewrite main loop so only blocking when necessary
    This allows the bot to be killed off by command from other protocols, since it normally won't be waiting on input from the console unless it has started receiving some.

--- src/DammitBot.Console/Program.cs
+++ src/DammitBot.Console/Program.cs
@@ -33,12 +33,24 @@ class Program
         try
         {
             bot.Run();
+            System.Console.Write("Message for bot: ");
 
             do
             {
-                System.Console.Write("Message for bot: ");
+                if (!System.Console.KeyAvailable)
+                {
+                    continue;
+                }
+                
+                // TODO: cancel console input after x amount of time so we don't block on accidental keypress
+
                 var message = System.Console.ReadLine() ?? string.Empty;
                 bot.ReceiveConsoleMessage(message);
+
+                if (bot.Running)
+                {
+                    System.Console.Write("Message for bot: ");
+                }
             } while (bot.Running);
         }
         catch (Exception e) when (LogAndRethrow(log, e))
