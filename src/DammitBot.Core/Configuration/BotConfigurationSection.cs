34363fee77f5817ab9280a002f5a84bd755140f7
Jason Duncan <functionreturnfunction@gmail.com>
2023-08-05 11:35:37 -0400

    Add documentation

--- src/DammitBot.Core/Configuration/BotConfigurationSection.cs
+++ src/DammitBot.Core/Configuration/BotConfigurationSection.cs
@@ -2,16 +2,40 @@
 
 namespace DammitBot.Configuration;
 
+/// <inheritdoc cref="IBotConfigurationSection"/>
 public class BotConfigurationSection : IBotConfigurationSection
 {
+    #region Constants
+    
+    /// <summary>
+    /// Key at which this configuration section can be found.
+    /// </summary>
     public const string KEY = "DammitBot";
+    
+    #endregion
+    
+    #region Private Members
 
     private readonly IConfigurationSection _config;
+    
+    #endregion
+    
+    #region Properties
 
+    /// <inheritdoc cref="IBotConfigurationSection.GoesBy"/>
     public string GoesBy => _config.EnsureConfigValue("goesBy");
+    
+    #endregion
+    
+    #region Constructors
 
+    /// <summary>
+    /// Constructor for the <see cref="BotConfigurationSection"/> class.
+    /// </summary>
     public BotConfigurationSection(IConfigurationSection config)
     {
         _config = config;
     }
+    
+    #endregion
 }
--- src/DammitBot.Core/Configuration/IBotConfigurationSection.cs
+++ src/DammitBot.Core/Configuration/IBotConfigurationSection.cs
@@ -1,6 +1,14 @@
 namespace DammitBot.Configuration;
 
+/// <summary>
+/// Configuration details pertaining to the bot itself.
+/// </summary>
 public interface IBotConfigurationSection
 {
+    /// <summary>
+    /// Name of the bot; it can assume it is being spoken to when found at the beginning of messages.
+    /// This value will be used in a <see cref="System.Text.RegularExpressions.Regex"/>, so the value can
+    /// contain valid regex syntax.
+    /// </summary>
     string GoesBy { get; }
 }
