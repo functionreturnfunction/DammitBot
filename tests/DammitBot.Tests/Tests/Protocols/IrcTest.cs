85e8ecba214cae85a974e81813ab37937f3a4893
Jason Duncan <functionreturnfunction@gmail.com>
2023-08-15 08:47:29 -0400

    More test coverage on Irc protocol

--- tests/DammitBot.Tests/Tests/Protocols/IrcTest.cs
+++ tests/DammitBot.Tests/Tests/Protocols/IrcTest.cs
@@ -1,5 +1,6 @@
 using System;
 using DammitBot.Configuration;
+using DammitBot.Events;
 using DammitBot.IoC;
 using DammitBot.Library;
 using DammitBot.Protocols;
@@ -26,6 +27,30 @@ public class IrcTest : UnitTestBase<Irc>
             .Mock();
         
         new IrcProtocolContainerConfiguration().Configure(serviceRegistry);
+    }
+
+    [Fact]
+    public void Test_Name_IsIrc()
+    {
+        Assert.Equal(nameof(Irc), _target.Name);
+    }
+
+    [Fact]
+    public void Test_ChannelMessageReceivedEvent_IsPassedAlong()
+    {
+        var message =
+            new MessageEventArgs("message", "channel", "protocol", "user");
+        var bubbled = false;
+
+        _target.ChannelMessageReceived += (_, args) => {
+            bubbled = true;
+            Assert.Equal(message, args);
+        };
+        _target.Initialize();
+        
+        _client!.Raise(x => x.ChannelMessageReceived += null, message);
+        
+        Assert.True(bubbled);
     }
 
     [Fact]
@@ -66,5 +91,75 @@ public class IrcTest : UnitTestBase<Irc>
         {
             _client.Verify(x => x.JoinChannel(channel));
         }
+    }
+
+    [Fact]
+    public void Test_SayToChannel_SendsMessageToChannel()
+    {
+        var message = "blah blah blah";
+        var channel = "#someChannel";
+        
+        _target.Initialize();
+        
+        _target.SayToChannel(channel, message);
+        
+        _client!.Verify(x => x.SendMessage(message, channel));
+    }
+
+    [Fact]
+    public void Test_SayToChannel_SplitsMessagesContainingNewlines()
+    {
+        var message = string.Format("blah{0}blah{0}blah", Environment.NewLine);
+        var channel = "#someChannel";
+        
+        _target.Initialize();
+        
+        _target.SayToChannel(channel, message);
+        
+        _client!.Verify(x => x.SendMessage("blah", channel), Times.Exactly(3));
+    }
+
+    [Fact]
+    public void Test_SayToChannel_Throws_WhenNotInitialized()
+    {
+        Assert.Throws<InvalidOperationException>(
+            () => _target.SayToChannel("foo", "bar"));
+    }
+
+    [Fact]
+    public void Test_SayToAll_SendsMessageToAllConfiguredChannels()
+    {
+        var configuration = _container.GetInstance<IOptions<IrcConfiguration>>().Value;
+        var message = "blah blah blah";
+        
+        _target.Initialize();
+        _client!.Raise(x => x.ReadyToJoinChannels += null, EventArgs.Empty);
+        
+        _target.SayToAll(message);
+
+        _client.Verify(x => x.SendMessage(message, configuration.Channels));
+    }
+
+    [Fact]
+    public void Test_SayToAll_SplitsMessagesContainingNewlines()
+    {
+        var configuration = _container.GetInstance<IOptions<IrcConfiguration>>().Value;
+        var message = string.Format("blah{0}blah{0}blah", Environment.NewLine);
+        
+        _target.Initialize();
+        _client!.Raise(x => x.ReadyToJoinChannels += null, EventArgs.Empty);
+        
+        _target.SayToAll(message);
+
+        _client.Verify(
+            x => x.SendMessage("blah", configuration.Channels),
+            Times.Exactly(3));
+    }
+
+    [Fact]
+    public void Test_SayToAll_Throws_WhenNotInitialized()
+    {
+        Assert.Throws<InvalidOperationException>(
+            () => _target.SayToAll("foo"));
     }
 }
