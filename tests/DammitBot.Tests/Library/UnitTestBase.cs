eaccf8ee8c11f699577935f29bca058ea1f75886
Jason Duncan <functionreturnfunction@gmail.com>
2023-09-03 21:20:16 -0400

    Found a hotspot by profiling
    Building the IoC container completely from scratch during the instance constructor of every unit test was a bit slow.

--- tests/DammitBot.Tests/Library/UnitTestBase.cs
+++ tests/DammitBot.Tests/Library/UnitTestBase.cs
@@ -12,6 +12,11 @@ public abstract class UnitTestBase<TTarget> : IDisposable
 {
     #region Private Members
 
+    // Scanning all the assemblies for types to register takes time, and only really needs to happen once
+    // for all the tests which inherit from this class.  Thus we build this static ServiceRegistry once
+    // from the static constructor and then use it repeatedly to build the container for each individual
+    // test in the instance constructor.
+    private static ServiceRegistry? _baseRegistry;
     protected readonly IContainer _container;
     protected TTarget _target;
     protected TestDateTimeProvider _dateTimeProvider;
@@ -23,11 +28,49 @@ public abstract class UnitTestBase<TTarget> : IDisposable
 
     protected virtual IContainer CreateContainer()
     {
-        return new Container(ConfigureContainer);
+        return new Container(serviceRegistry => {
+            serviceRegistry.IncludeRegistry(_baseRegistry);
+            ConfigureContainer(serviceRegistry);
+        });
     }
 
     protected virtual void ConfigureContainer(ServiceRegistry serviceRegistry)
     {
+        _dateTimeProvider = serviceRegistry
+            .For<IDateTimeProvider>()
+            .Use<IDateTimeProvider, TestDateTimeProvider>(
+                new TestDateTimeProvider(_now = DateTime.Now));
+    }
+
+    protected virtual void ExtraSetup() {}
+
+    protected virtual TTarget CreateTarget()
+    {
+        return _container.GetInstance<TTarget>();
+    }
+
+    #endregion
+
+    #region Setup/Teardown
+
+    static UnitTestBase()
+    {
+        _baseRegistry ??= CreateBaseRegistry();
+    }
+
+    public UnitTestBase()
+    {
+        _container = CreateContainer();
+
+        ExtraSetup();
+
+        _target = CreateTarget();
+    }
+
+    private static ServiceRegistry CreateBaseRegistry()
+    {
+        var serviceRegistry = new ServiceRegistry();
+        
         serviceRegistry.Scan(a => {
             a.AssembliesFromApplicationBaseDirectory();
             a.WithDefaultConventions();
@@ -46,30 +89,8 @@ public abstract class UnitTestBase<TTarget> : IDisposable
             .ValidateDataAnnotations();
 
         serviceRegistry.For<ILogger<TTarget>>().Mock();
-        _dateTimeProvider = serviceRegistry
-            .For<IDateTimeProvider>()
-            .Use<IDateTimeProvider, TestDateTimeProvider>(
-                new TestDateTimeProvider(_now = DateTime.Now));
-    }
-
-    protected virtual void ExtraSetup() {}
-
-    protected virtual TTarget CreateTarget()
-    {
-        return _container.GetInstance<TTarget>();
-    }
-
-    #endregion
-
-    #region Setup/Teardown
-
-    public UnitTestBase()
-    {
-        _container = CreateContainer();
-
-        ExtraSetup();
-
-        _target = CreateTarget();
+        
+        return serviceRegistry;
     }
 
     public virtual void Dispose()
